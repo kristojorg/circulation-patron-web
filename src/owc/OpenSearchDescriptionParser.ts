ba68435a9d93724f674049ffa6904542cd2d66d0
kristojorg <kristo.jorgenson@gmail.com>
2020-09-11 19:21:37 +0200

    auth plugin

--- src/owc/OpenSearchDescriptionParser.ts
+++ src/owc/OpenSearchDescriptionParser.ts
@@ -2,23 +2,23 @@ import { SearchData } from "./interfaces";
 import * as xml2js from "xml2js";
 import * as url from "url";
 
-let xmlParser = new xml2js.Parser({ xmlns: true });
+const xmlParser = new xml2js.Parser({ xmlns: true });
 
 /** Converts an open search description document into the application's internal
     representation. */
 export default class OpenSearchDescriptionParser {
   parse(xml: string, descriptionUrl: string): Promise<SearchData> {
     return new Promise((resolve, reject) => {
-      xmlParser.parseString(xml, (err, result) => {
+      xmlParser.parseString(xml, (err: any, result: any) => {
         if (err) {
           reject(err);
         } else {
           if (result.OpenSearchDescription) {
-            let root = result.OpenSearchDescription;
-            let description = root["Description"][0]["_"];
-            let shortName = root["ShortName"][0]["_"];
-            let templateString = root["Url"][0]["$"].template.value;
-            let template = (s: string) => {
+            const root = result.OpenSearchDescription;
+            const description = root["Description"][0]["_"];
+            const shortName = root["ShortName"][0]["_"];
+            const templateString = root["Url"][0]["$"].template.value;
+            const template = (s: string) => {
               return url.resolve(
                 descriptionUrl,
                 templateString.replace("{searchTerms}", s)
