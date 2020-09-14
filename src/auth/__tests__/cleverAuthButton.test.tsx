9630a5e803a47f08cadac3952316bd823677fe99
Monica Powell <monica@aboutmonica.com>
2020-09-14 11:25:04 -0400

    make import statement less verbose

--- src/auth/__tests__/cleverAuthButton.test.tsx
+++ src/auth/__tests__/cleverAuthButton.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render } from "../../../src/test-utils";
+import { render } from "../../test-utils";
 import { CleverButton, getAuthUrl } from "../cleverAuthButton";
 
 describe("CleverButton", () => {
