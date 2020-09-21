ad1ad37c592c513ce725d96ec7a4cc2f17f968f7
kristojorg <kristo.jorgenson@gmail.com>
2020-09-21 23:25:49 +0200

    fix clever button

--- src/auth/cleverAuthButton.tsx
+++ src/auth/cleverAuthButton.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Button from "components/Button";
+import Button, { AnchorButton } from "components/Button";
 
 import { modalButtonStyles } from "components/Modal";
 import { OPDS1 } from "interfaces";
@@ -9,31 +9,31 @@ const CleverButton: React.FC<{ method: OPDS1.CleverAuthMethod }> = ({
 }) => {
   const currentUrl = window.location.origin + window.location.pathname;
   const imageUrl = method.links?.find(link => link.rel === "logo")?.href;
-  // double encoding is required for unshortened book urls to be redirected to properly
-  const authUrl = `${
-    method.links?.find(link => link.rel === "authenticate")?.href
-  }&redirect_uri=${encodeURIComponent(encodeURIComponent(currentUrl))}`;
 
-  function saveCredentials() {
-    window.location.href = authUrl;
-  }
+  const authenticateHref = method.links?.find(
+    link => link.rel === "authenticate"
+  )?.href;
+  // double encoding is required for unshortened book urls to be redirected to properly
+  const authUrl = authenticateHref
+    ? `${authenticateHref}&redirect_uri=${encodeURIComponent(
+        encodeURIComponent(currentUrl)
+      )}`
+    : undefined;
 
   return authUrl ? (
-    <a href={authUrl}>
-      <Button
-        onClick={saveCredentials}
-        type="submit"
-        sx={{
-          ...modalButtonStyles,
-          color: "#ffffff",
-          backgroundColor: "#2f67aa",
-          backgroundImage: `url(${imageUrl})`
-        }}
-        aria-label="Log In with Clever"
-      >
-        {!imageUrl ? "Log In With Clever" : ""}
-      </Button>
-    </a>
+    <AnchorButton
+      href={authUrl}
+      type="submit"
+      sx={{
+        ...modalButtonStyles,
+        color: "#ffffff",
+        backgroundColor: "#2f67aa",
+        backgroundImage: `url(${imageUrl})`
+      }}
+      aria-label="Log In with Clever"
+    >
+      {!imageUrl ? "Log In With Clever" : ""}
+    </AnchorButton>
   ) : null;
 };
 
