4e916b0d22a2279e85198414e79a18394510e3cc
kristojorg <kristo.jorgenson@gmail.com>
2020-09-28 18:22:00 +0200

    test change

--- src/auth/__tests__/CleverButton.test.tsx
+++ src/auth/__tests__/CleverButton.test.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
-import { render, fixtures } from "test-utils";
+import { fixtures } from "test-utils";
 import CleverButton from "../CleverButton";
+
+// we import the unwrapped render here because we don't need the context providers
+import { render } from "@testing-library/react";
 
 describe("CleverButton", () => {
   test("constructs proper authUrl", () => {
