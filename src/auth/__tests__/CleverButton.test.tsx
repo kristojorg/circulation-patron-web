17f99b0fd3535a7e6a1a9c4040cdecfc35c37b2f
kristojorg <kristo.jorgenson@gmail.com>
2020-09-28 18:17:06 +0200

    type fixes

--- src/auth/__tests__/CleverButton.test.tsx
+++ src/auth/__tests__/CleverButton.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render } from "test-utils";
+import { render, fixtures } from "test-utils";
 import CleverButton from "../CleverButton";
 
 describe("CleverButton", () => {
