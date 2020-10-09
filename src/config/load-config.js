520609a5aefceabe71e8a8a9d80921484d5418a0
kristojorg <kristo.jorgenson@gmail.com>
2020-10-09 14:59:35 +0200

    reload app config on change

--- src/config/load-config.js
+++ src/config/load-config.js
@@ -1,4 +1,5 @@
 const getAppConfig = require("./fetch-config");
+const path = require("path");
 /**
  * We use webpack's val-loader to run this file at build time so when
  * you require "load-config.js" it will be the value of the
@@ -6,9 +7,12 @@ const getAppConfig = require("./fetch-config");
  */
 module.exports = async () => {
   const config = await getAppConfig(process.env.CONFIG_FILE);
+  const configFilePath = path.join(process.cwd(), process.env.CONFIG_FILE);
   return {
     code: `module.exports = ${JSON.stringify(config)}`,
     // the value will be cached in development, not refetched on every build
-    cacheable: true
+    // unless the config file changes
+    cacheable: true,
+    dependencies: [configFilePath]
   };
 };
